2355a114e61ab015e8d5ad26e25faa7f89083953
Harry Ho <TheTitan8706@gmail.com>
2015-05-31 16:16:41 -0400

    feat(FooterBlock) adds default FooterBlock component with base style

---
+++ app/lib/footer/index.js
@@ -0,0 +1,15 @@
+import './footer.scss';
+import React from 'react';
+
+export default class FooterBlock extends React.Component {
+  constructor (props) {
+    super(props);
+  }
+  render () {
+    return (
+      <div className='footer-block'>
+        This is the footer
+      </div>
+    );
+  }
+}
