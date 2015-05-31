f84c870b203e9802acfad732987a4600f0e61c8a
Harry Ho <TheTitan8706@gmail.com>
2015-05-31 16:12:01 -0400

    refactor(entry) updates entry to include a React render of Welcome component

--- app/index.js
+++ app/index.js
@@ -1,8 +1,20 @@
+//Get React
 import React from 'react';
 
+//Polyfill and base welcome Component
 import 'babel/polyfill';
-import Welcome from './lib/welcome/index.js';
+import Welcome from 'lib/welcome';
 
+//Import Base Style
+import 'styles/app.scss';
+
+//Grab the Application Element to build on
 const appElement = document.getElementById('app');
 
-React.render(<Welcome message='Welcome to the Application'/>, appElement);
+//Render Default
+React.render(
+  <div>
+    <Welcome message='Welcome to the Application'>
+    </Welcome>
+  </div>
+  , appElement);
