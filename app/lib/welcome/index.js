7f1956cc2a1745190a848b992acbdaf61d4b1a57
Harry Ho <TheTitan8706@gmail.com>
2015-05-31 16:12:39 -0400

    refactor(welcome) adds more flesh out Welcome component

--- app/lib/welcome/index.js
+++ app/lib/welcome/index.js
@@ -1,4 +1,6 @@
-const React = require('react');
+import React from 'react';
+import ContentBlock from 'lib/content';
+import FooterBlock from 'lib/footer';
 
 export default class Welcome extends React.Component {
   constructor (props) {
@@ -9,6 +11,8 @@ export default class Welcome extends React.Component {
       <div>
         <h2> {this.props.message}</h2>
         <p> This message has been brought to you buy fishes </p>
+        <ContentBlock />
+        <FooterBlock />
       </div>
     );
   }
